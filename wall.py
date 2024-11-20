f58eb446bb7f535ad62572a36579ff85c0f89dc3
susan <susan.mei@qq.com>
2024-11-20 09:57:42 +0800

    real wall

---
+++ wall.py
@@ -0,0 +1,10 @@
+import pygame
+import config
+
+class Wall(pygame.sprite.Sprite):
+    def __init__(self, x, y, width, height):
+        super().__init__()
+        self.image = pygame.Surface((width, height))
+        self.image.fill((255, 0, 0))
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
