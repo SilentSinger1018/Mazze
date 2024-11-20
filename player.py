f60078c19026c1b2bfff26fe55887b89e58f1251
susan <susan.mei@qq.com>
2024-11-20 09:30:31 +0800

    finish the car move

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 import pygame
 import config
+import math
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, center_x=config.SCREEN_WIDTH / 2, center_y=config.SCREEN_HEIGHT / 2):
@@ -13,7 +14,14 @@ class Player(pygame.sprite.Sprite):
         self.rect.center = (center_x, center_y)
         self.last_time = pygame.time.get_ticks()
         self.delta_time = 0
-        self.move_velocity = 0
+        self.move_acc = 600 #加速度
+        self.move_velocity = 0 #当前的移动速度
+        self.move_velocity_limit = 220 #移动速度的上限
+        self.smallest_rotate_move = 30 #使得车子可以克服摩擦力转动的最小角速度
+        self.rotate_velocity = 0 
+        self.rotate_velocity_limit = 140 #角速度移动上限
+        self.forward_angle = 0 #车身与水平线的角度
+        self.friction = 0.9
 
     def update_delta_time(self):
         current_time = pygame.time.get_ticks()
@@ -22,14 +30,45 @@ class Player(pygame.sprite.Sprite):
 
     def input(self):
         key_pressed = pygame.key.get_pressed()
-        if key_pressed[pygame.K_w]:
-            self
+        if key_pressed[pygame.K_UP]:
+            self.move_velocity += self.move_acc * self.delta_time
+            self.move_velocity = min(self.move_velocity, self.move_velocity_limit)
+        elif key_pressed[pygame.K_DOWN]:
+            self.move_velocity -= self.move_acc * self.delta_time
+            self.move_velocity = max(self.move_velocity, -self.move_velocity_limit)
+        else:
+            self.move_velocity = self.move_velocity * self.friction
+
+        sign = 1
+        if(self.move_velocity < 0):
+            sign = -1
+
+        if key_pressed[pygame.K_RIGHT]:
+            self.rotate_velocity = sign * self.rotate_velocity_limit
+        elif key_pressed[pygame.K_LEFT]:
+            self.rotate_velocity = -sign * self.rotate_velocity_limit
+        else:
+            self.rotate_velocity = 0
+                
+    def rotate(self):
+        self.forward_angle += self.rotate_velocity * self.delta_time
+        self.image = pygame.transform.scale(self.image_source, (self.width, self.height))
+        self.image = pygame.transform.rotate(self.image, -self.forward_angle)
+        self.image.set_colorkey("black")
+        #需要按照中心点旋转，所以旋转前后的中心点不会发生变化
+        center = self.rect.center
+        self.rect = self.image.get_rect()
+        self.rect.center = center
 
     def move(self):
-        vx = self.move_velocity * self.delta_time
-        self.rect.x += vx
+        if(abs(self.move_velocity) > self.smallest_rotate_move):
+            self.rotate()
+        vx = self.move_velocity * math.cos(math.pi * self.forward_angle / 180)
+        vy = self.move_velocity * math.sin(math.pi * self.forward_angle / 180)
+        self.rect.x += vx * self.delta_time 
+        self.rect.y += vy * self.delta_time
 
     def update(self):
         self.update_delta_time()
+        self.input()
         self.move()
-
